1daa8b825adb9a5d4ce15862315468c9151016d4
Jack Haines <jack.r.haines@outlook.com>
2019-04-18 15:25:19 +0100

    Model Added

---
+++ CSIMediaTest/Models/Sequence.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace CSIMediaTest.Models
+{
+    public class Sequence
+    {
+        public int ID { get; set; }
+        public int NewSequence { get; set; }
+        public string Direction { get; set; }
+        public DateTime TimeTaken { get; set; }
+    }
+
+    public class SequenceDBContext : DbContext
+    {
+        public DbSet<Sequence> Sequences { get; set; }
+    }
+}
