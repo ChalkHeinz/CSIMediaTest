88e9dce5afcfc7f0b194c5b4ffd8d5add8ca1339
Jack Haines <jack.r.haines@outlook.com>
2019-04-21 14:42:54 +0100

    Updated the sorting and added viewmodel

--- CSIMediaTest/Controllers/HomeController.cs
+++ CSIMediaTest/Controllers/HomeController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Dynamic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 using CSIMediaTest.Models;
+using CSIMediaTest.ViewModels;
 
 namespace CSIMediaTest.Controllers
 {
@@ -22,13 +26,32 @@ namespace CSIMediaTest.Controllers
         {
             var sequence = Request.Form[0];
             var order = Request.Form[1];
+            Stopwatch stopwWatch = new Stopwatch();
+            double seconds = 0;
 
-            //Sort's runtime 
-            var startTime = DateTime.Now;
-            sequence = String.Concat(sequence.OrderBy(c => c));
-            var endTme = DateTime.Now;
+            //Prep sequence data
+            string[] temp = sequence.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
 
-            double seconds = (endTme - startTime).TotalSeconds;
+            if (order == "ascending")
+            {
+                //Sort's runtime 
+                stopwWatch.Start();
+                Array.Sort(temp);
+                stopwWatch.Stop();
+            }
+            else if (order == "descending")
+            {
+                //Sort's runtime 
+                stopwWatch.Start();
+                Array.Sort(temp);
+                stopwWatch.Stop();
+                temp.Reverse();
+                
+            }
+
+            seconds = stopwWatch.Elapsed.TotalMilliseconds;
+            //End of manipulation of sequence data
+            sequence = String.Join(" ", temp);
 
             Sequence sequenceObject = new Sequence {
                 NewSequence = sequence,
@@ -39,14 +62,19 @@ namespace CSIMediaTest.Controllers
             dBContext.Sequences.Add(sequenceObject);
             dBContext.SaveChanges();
 
-            return RedirectToAction("SequenceList");
+            return RedirectToAction("SequenceList", sequenceObject);
 
         }
 
-        [AcceptVerbs(HttpVerbs.Get)]
-        public ActionResult SequenceList()
+        
+        public ActionResult SequenceList(Sequence sequenceObject)
         {
-            return View();
+            SequenceResultViewModel sequenceResultViewModel = new SequenceResultViewModel();
+
+            sequenceResultViewModel.Sequences = dBContext.Sequences.ToList();
+            sequenceResultViewModel.NewSequence = sequenceObject;
+
+            return View(sequenceResultViewModel);
         }
 
     }
---
+++ CSIMediaTest/ViewModels/SequenceResultViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CSIMediaTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSIMediaTest.ViewModels
+{
+    public class SequenceResultViewModel
+    {
+        public IList<Sequence> Sequences{ get; set;}
+        public Sequence NewSequence { get; set; }
+    }
+}
