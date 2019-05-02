c91096b7443383cd53ff2f61688e744160c7a39e
Jack Haines <jack.r.haines@outlook.com>
2019-05-02 15:08:38 +0100

    Initialize unit testing

---
+++ CSIMediaTestTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSIMediaTest.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSIMediaTest.Controllers.Tests
+{
+    [TestClass()]
+    public class HomeControllerTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void OrderSequenceTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ CSIMediaTestTests/Controllers/SequenceListControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSIMediaTest.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSIMediaTest.Controllers.Tests
+{
+    [TestClass()]
+    public class SequenceListControllerTests
+    {
+        [TestMethod()]
+        public void SequenceListTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ExportTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
