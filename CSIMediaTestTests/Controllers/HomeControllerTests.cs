c7187bda9596c01fe0debf1f17619889fc00f8c4
Jack Haines <jack.r.haines@outlook.com>
2019-05-02 16:09:03 +0100

    Unit Testing: Added order sequence tests

--- CSIMediaTestTests/Controllers/HomeControllerTests.cs
+++ CSIMediaTestTests/Controllers/HomeControllerTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CSIMediaTest.Models;
 
 namespace CSIMediaTest.Controllers.Tests
 {
@@ -24,9 +25,33 @@ namespace CSIMediaTest.Controllers.Tests
         }
 
         [TestMethod()]
-        public void OrderSequenceTest()
+        public void OrderSequenceTest_AscendingOrder_ReturnsAscendingOrder()
         {
-            Assert.Fail();
+            //Arrange
+            var sequence = "5 4 6 2 1";
+            var newSequence = "1 2 4 5 6";
+            var controller = new HomeController();
+
+            //Act
+            var result = controller.OrderSequence(sequence, Directions.Ascending);
+
+            //Asert
+            Assert.AreEqual(newSequence, result.Item2);
+        }
+
+        [TestMethod()]
+        public void OrderSequenceTest_DescendingOrder_ReturnsDescendingOrder()
+        {
+            //Arrange
+            var sequence = "5 4 6 2 1";
+            var newSequence = "6 5 4 2 1";
+            var controller = new HomeController();
+
+            //Act
+            var result = controller.OrderSequence(sequence, Directions.Descending);
+
+            //Asert
+            Assert.AreEqual(newSequence, result.Item2);
         }
     }
 }
